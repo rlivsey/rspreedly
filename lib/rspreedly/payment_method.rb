c5d8de89cdbfd7077d03eacb965b8851be905f36
Richard Livsey <richard@livsey.org>
2009-11-03 22:06:10 +0100

    added support for addresses in payments

--- lib/rspreedly/payment_method.rb
+++ lib/rspreedly/payment_method.rb
@@ -1,7 +1,10 @@
 module RSpreedly
   module PaymentMethod
     class CreditCard < Base
-      attr_accessor :number, :verification_value, :month, :year, :first_name, :last_name, :card_type
+      attr_accessor :number, :verification_value, :month, 
+                    :year, :first_name, :last_name, :card_type,
+                    :address1, :address2, :city, :state, :zip, :country,
+                    :phone_number
     end
   end
 end
