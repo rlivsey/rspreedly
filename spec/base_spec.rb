ed96a49a2bea919be9ede3ea6e448e98b24475d9
Richard Livsey <richard@livsey.org>
2009-11-03 22:21:53 +0100

    Fix typo in base spec

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -2,11 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe RSpreedly::Base do
 
   describe "#api_request" do
-    it "should raise AccessDenied if a 401 is receiver" do
+    it "should raise AccessDenied if a 401 is received" do
       stub_http_with_code(401)
       lambda{
         RSpreedly::Base.api_request(:put, '/')
       }.should raise_error(RSpreedly::Error::AccessDenied)
     end
+    
   end
 end
