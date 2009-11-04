e6ac5f670eabc93d5cb8b89122f66df692b1eeaf
Richard Livsey <richard@livsey.org>
2009-11-04 01:04:26 +0100

    Remove net-http-spy from spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'spec'
-require 'net-http-spy'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rspreedly'
